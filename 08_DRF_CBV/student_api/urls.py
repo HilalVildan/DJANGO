067c9d4ed0ce8f6ad6d167e55d9de2ef5377750c
HilalVildan <hilalsarioglu96@gmail.com>
2022-12-15 12:24:40 +0100

    class added

---
+++ 08_DRF_CBV/student_api/urls.py
@@ -0,0 +1,64 @@
+from django.urls import path, include
+
+# --- FBV IMPORTS -----
+'''
+from .views import (
+    home,
+    student_list,
+    student_create,
+    student_detail,
+    student_update,
+    student_delete,
+    student_list_create,
+    student_detail_update_delete,
+)
+'''
+
+# --- CBV IMPORTS -----
+from .views import (
+    home,
+    StudentListCreate,
+    StudentDetail,
+    StudentGAV,
+    StudentDetailGAV,
+    StudentCV,
+    StudentDetailCV,
+    StudentMVS
+)
+
+#ModelViewSet icin bunu yapiyoruz ve urlpatterns de sadece bir kod yaziyoruz her seyi calistiriyor.
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('student', StudentMVS)
+
+# after /api/:
+# ----------- FBV URLS --------------
+'''
+urlpatterns = [
+    path('', home),
+    path('student_list/', student_list),
+    path('student_create/', student_create),
+    path('student_detail/<int:pk>', student_detail),
+    path('student_update/<int:pk>', student_update),
+    path('student_delete/<int:pk>', student_delete),
+    # concat_functions()
+    path('student_list_create/', student_list_create),
+    path('student_detail_update_delete/<int:pk>', student_detail_update_delete),
+]
+'''
+
+
+
+urlpatterns = [
+    path('', home),
+    # path('student/', StudentListCreate.as_view()),
+    # path('student/', StudentGAV.as_view()),
+    # path('student/', StudentCV.as_view()),
+
+    # path('student/<int:pk>/', StudentDetail.as_view())
+    # path('student/<int:pk>/', StudentDetailGAV.as_view())
+    # path('student/<int:pk>/', StudentDetailCV.as_view())
+
+
+path('', include(router.urls)) # ModelViewSet icin router islemi ile bu kod yeterli...
+]
